7a4f9c26df9d4ea16be11f8d61991143312ab25d
e6i24v <e6i24v@adachitoorumeinoMacBook-Pro.local>
2022-08-07 17:30:40 +0900

    modify Data class 削除 posgres への接続を　before do endへ パラメータをSQL文に直接入れていたが、変更

--- app.rb
+++ app.rb
@@ -2,12 +2,47 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require './db/database'
 require 'sinatra/base'
+require 'pg'
+
+before do
+  @connection = PG.connect(host: 'localhost', port: 5432, dbname: 'memo')
+  @connection.exec('CREATE TABLE IF NOT EXISTS memos (id serial PRIMARY KEY,title varchar(40),content text) ')
+end
+
+def all_memos
+  query = 'SELECT * from memos'
+
+  dbmemos = @connection.exec(query)
+  dbmemos.map do |row|
+    { id: row['id'].to_s, title: row['title'], content: row['content'] }
+  end
+end
+
+def insert_memos(memo)
+  query = 'INSERT INTO memos (title,content) VALUES($1,$2)'
+  @connection.exec(query, [memo[:title], memo[:content]])
+end
+
+def find_memo(params)
+  query = 'SELECT id,title, content FROM memos WHERE id = $1'
+  dbmemos = @connection.exec(query, [params[:id]])
+  dbmemos.first
+end
+
+def update_memo(params)
+  query = 'UPDATE memos SET title = $2 ,content= $3 WHERE id = $1'
+  @connection.exec(query, [params[:id].to_i, params[:title], params[:content]])
+end
+
+def delete_memo(params)
+  query = 'DELETE from memos WHERE id = $1'
+  @connection.exec(query, [params[:id]])
+end
 
 get '/' do
   @title = 'top'
-  @memos = Data.all
+  @memos = all_memos
   erb :top
 end
 
@@ -18,39 +53,32 @@ end
 
 get '/memos/:id' do
   @title = 'show'
-  @memo = Data.find_memo(h(params[:id]).to_s)
+  @memo = find_memo(params)
   erb :show
 end
 
 get '/memos/:id/edit' do
   @title = 'edit'
-  @memo = {}
-  @memo = Data.find_memo(h(params[:id]).to_s)
+  @memo = find_memo(params)
   erb :edit
 end
 
 post '/memos' do
-  memo = { 'title' => h(params[:title]).to_s, 'content' => h(params[:content]).to_s }
-  Data.insert_memos(memo)
+  memo = { title: params['title'], content: params['content'] }
+  insert_memos(memo)
   redirect to('/')
 end
 
 patch '/memos/:id' do
-  Data.update_memo(h(params[:id]).to_s, h(params[:title]).to_s, h(params[:content]).to_s)
+  update_memo(params)
   redirect to('/')
 end
 
 delete '/memos/:id' do
-  Data.delete_memo(h(params[:id]).to_s)
+  delete_memo(params)
   redirect to('/')
 end
 
 get '/*' do
   erb :notfound
-end
-
-helpers do
-  def h(text)
-    Rack::Utils.escape_html(text)
-  end
 end
